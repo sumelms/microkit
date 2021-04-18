cad1aec9ac6f9e9c326e32a422409321f3cdec43
Ricardo Lüders <rluders@redhat.com>
2021-04-18 12:42:26 +0200

    Fix encoding issue to health checker

--- http/handler/health.go
+++ http/handler/health.go
@@ -5,7 +5,7 @@ import "net/http"
 // HealthCheckerHandler service health
 func HealthCheckerHandler() http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte("OK"))
+		rw.Write([]byte("{\"Status\": \"OK\"}"))
 		rw.WriteHeader(200)
 		return
 	})
